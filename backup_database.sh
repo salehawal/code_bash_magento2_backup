467f053c2d220a83f2078e0bcd8e063e9e4f03b2
Saleh El-Jabarty <saleh.awal@gmail.com>
2019-10-27 12:49:15 +0200

    compressed database backup

--- backup_database.sh
+++ backup_database.sh
@@ -7,6 +7,9 @@
 
 # backup database
 mysqldump --add-drop-table --force --opt $database_name > ${database_backup_to}/${database_export_file}_`date +%y-%m-%d_%H-%M`.sql
+# Compress Files
+tar -czvf ${database_backup_to}/${database_export_file}_`date +%y-%m-%d_%H-%M`.tar.bz2 ${database_backup_to}/${database_export_file}_`date +%y-%m-%d_%H-%M`.sql
+rm ${database_backup_to}/${database_export_file}_`date +%y-%m-%d_%H-%M`.sql
 # remove old backups
 find $backup_to -name ${database_export_file}_* -type f -mmin +7200 -delete
 # send notification email
